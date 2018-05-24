0ea7f6fa014da9daf78797cf53bf456d0582c20e
Alex Barker <alexbark@gmail.com>
2018-05-24 19:34:59 +1000

    Add project files.

---
+++ multi-thread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multi_thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
